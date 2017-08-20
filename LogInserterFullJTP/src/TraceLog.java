a4e55a2606e06fe515ede54a42238360d698d40f
joe <yongzhiwang@icloud.com>
2017-08-20 05:26:59 -0700

    Initial version

---
+++ LogInserterFullJTP/src/TraceLog.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TraceLog {
+	
+	public TraceLog(String line) {
+		
+		String[] parts = line.split("\t");
+		params = new HashMap<String, String>();
+		
+		for(int i = 1; i<parts.length;i+=2)
+			params.put(parts[i],parts[i+1]);
+	}
+
+	Map<String, String> params;
+	
+	public Map<String, String> getParams(){
+		return params;
+	}
+
+}
