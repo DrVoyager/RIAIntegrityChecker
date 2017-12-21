a6938a01d8202f192d074aa2ad858f8189aec4a4
DIKDIK <mia443069185@gmail.com>
2017-12-21 10:08:20 +0800

    add ant build.xml and modified some sh file

--- examples/clean_workspace.sh
+++ examples/clean_workspace.sh
@@ -2,3 +2,18 @@ rm *.jar
 rm -rf hadoop-*
 rm -rf collectedLogs
 rm -rf sootOutput
+
+WORK_SPACE=/home/joe/Development/git/IntegrityCheck/examples
+LOGGER_LIB_DIR=$WORK_SPACE/../LoggerLibrary
+INSERTER_DIR=$WORK_SPACE/../LogInserterFullJTP
+CHECKER_DIR=$WORK_SPACE/../IntegrityChecker
+
+cd $LOGGER_LIB_DIR
+ant clean
+
+cd $INSERTER_DI
+ant clean
+
+cd $CHECKER_DIR
+ant clean
+
